8f4868e51aaac22cb8c61e43a0c2b67830737ef8
cristian parra <pacc2430@gmail.com>
2022-10-30 21:29:03 -0500

    se agrega index y script para validar conexión js

---
+++ script.js
@@ -0,0 +1,15 @@
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const pClass = document.getElementsByClassName('pClass');
+const pId = document.getElementById('pId');
+const input =document.querySelector('input');
+
+console.log(input.value);
+
+console.log({
+    h1,
+    p,
+    pClass,
+    pId,
+    input
+})
