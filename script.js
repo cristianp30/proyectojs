7c111952c59a9d5d2c2c72b2188fbdeab6817b87
cristian parra <pacc2430@gmail.com>
2022-11-01 22:30:16 -0500

    se agrega addevenlistener y se limpia el html

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const input2 = document.querySelector('#calcular2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
 
+btn.addEventListener('click', btnOnClick);
+
 function btnOnClick() {
     const sumaInputs = input1.value + input2.value;
     pResult.innerText = "The result is " + sumaInputs;
