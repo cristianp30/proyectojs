98ef8472db9e944c36299cf34f03a7fc2976ea52
cristian parra <pacc2430@gmail.com>
2022-11-05 23:48:34 -0500

    se agrega form en html y se crea el preventDefault para el evento submit

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const h1 = document.querySelector('h1');
+const form1 = document.querySelector('#form');
 const input1 = document.querySelector('#calcular1');
 const input2 = document.querySelector('#calcular2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
 
-btn.addEventListener('click', btnOnClick);
-
-function btnOnClick() {
+form1.addEventListener('submit', formulario);
+function formulario(event) {
+    console.log({event});
+    event.preventDefault();
     const sumaInputs = input1.value + input2.value;
     pResult.innerText = "The result is " + sumaInputs;
 } 
